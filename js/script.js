7b1f8b134a97c023526e92bffe158b6ba1669488
Andrei Kaimanov <an.cayman@ya.ru>
2021-10-19 16:58:25 +0200

    feat: added showGreeting() & enterName()

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString('en-US', { hour12: false });
     classTime.textContent = currentTime;
-}
+};
 //-- set time
 //-- set date
 const classDate = document.querySelector('.date');
@@ -15,15 +15,61 @@ function showDate() {
     const currentMonth = date.toLocaleDateString('en-US', {month: 'long'});
     const currentDay = date.toLocaleDateString('en-US', {day: 'numeric'});
     classDate.textContent = `${currentWeekday}, ${currentMonth} ${currentDay}`;
-}
+};
 //-- set date
+//-- set greeting
+const classGreeting = document.querySelector('.greeting');
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    switch(hours) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            return 'Night';
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            return 'Morning';
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+            return 'Day';
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+            return 'Evening';
+    };
+};
+function showGreeting() {
+    const timeOfDay = getTimeOfDay();
+    const greetingText = `Good ${timeOfDay},`;
+    classGreeting.textContent = greetingText;
+};
+//-- set greeting
 //-- every second interval
 setInterval(() => {
     showTime();
     showDate();
+    showGreeting();
 }, 1000);
 //-- every second interval
-
-
-// console.log(currentTime);
-// console.log(time);
+//-- set name
+const className = document.querySelector('.name');
+function enterName() {
+    className.placeholder='[Enter Name]';
+};
+enterName();
+//-- set name

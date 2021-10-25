efdb92394f8b7ecbe90f3c373009d9db8da2a258
Andrei Kaimanov <an.cayman@ya.ru>
2021-10-25 10:32:00 +0200

    added json quotes

--- js/script.js
+++ js/script.js
@@ -74,32 +74,97 @@ function enterName() {
 enterName();
 //-- set placeholder
 //-- set Name
-const inputName = document.querySelector('div.greeting-container > input');
+const inputName = document.querySelector('.name');
+const inputCity = document.querySelector('.city');
 function setLocalStorage() {
     localStorage.setItem('name', inputName.value);
-}
+    localStorage.setItem('city', inputCity.value);
+};
 window.addEventListener('beforeunload', setLocalStorage);
 
 function getLocalStorage() {
     if (localStorage.getItem('name')) {
         inputName.value = localStorage.getItem('name');
+        inputCity.value = localStorage.getItem('city');
     }
-}
+};
 window.addEventListener('load', getLocalStorage);
 //-- set Name
 //-- set BG
 function getRandomNum() {
     return Math.floor(Math.random() * 19) + 1;
-}
+};
 
+const timeOfDay = getTimeOfDay();
+const body = document.querySelector('body');
 function setBg() {
-    const timeOfDay = getTimeOfDay();
     let bgNum = getRandomNum();
     if (bgNum < 10) {
         bgNum = '0' + bgNum;
     }
-    const body = document.querySelector('body');
     body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay.toLowerCase()}/${bgNum}.jpg`;
-}
-setBg()
+};
+setBg();
 //-- set BG
+//-- bg slider
+let randomNum = getRandomNum();
+function getSlideNext() {
+    if (randomNum < 10) {
+        randomNum = '0' + randomNum;
+    }
+    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay.toLowerCase()}/${randomNum}.jpg`;
+    if (randomNum == 20) {
+        randomNum = 0;
+    }
+    randomNum ++;
+};
+
+function getSlidePrev() {
+    if (randomNum < 10) {
+        randomNum = '0' + randomNum;
+    }
+    body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay.toLowerCase()}/${randomNum}.jpg`;
+    if (randomNum == 1) {
+        randomNum = 21;
+    }
+    randomNum --;
+};
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+//-- bg slider
+//-- weather
+// const weatherIcon = document.querySelector('.weather-icon');
+// const temperature = document.querySelector('.temperature');
+// const weatherDescription = document.querySelector('.weather-description');
+// async function getWeather() {
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.textContent}lang=en&appid=68bf2022bf776ecd194328a3ad6537f6&units=metric`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+
+//     weatherIcon.className = 'weather-icon owf';
+//     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+//     temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+//     weatherDescription.textContent = data.weather[0].description;
+// }
+
+// function setCity(event) {
+//     if (event.code === 'Enter') {
+//         getWeather();
+//         inputCity.blur();
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', getWeather);
+// inputCity.addEventListener('keypress', setCity);
+//-- weather
+//-- quotes
+async function getQuotes() {  
+    const data = '../assets/json/quotes.json';
+    const res = await fetch(data);
+    const quotes = await res.json(); 
+    console.log(quotes);
+}
+getQuotes();
+//-- quotes

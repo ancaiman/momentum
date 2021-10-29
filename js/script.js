bcc7b45331f2bb9dfa25de87121d6db09ae90e12
Andrei Kaimanov <an.cayman@ya.ru>
2021-10-29 15:30:46 +0200

    refactor: getTimeOfDay func

--- js/script.js
+++ js/script.js
@@ -20,39 +20,13 @@ function showDate() {
 //-- set greeting
 const classGreeting = document.querySelector('.greeting');
 function getTimeOfDay() {
-    const date = new Date();
-    const hours = date.getHours();
-    switch(hours) {
-        case 0:
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            return 'Night';
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-        case 11:
-            return 'Morning';
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-        case 16:
-        case 17:
-            return 'Afternoon';
-        case 18:
-        case 19:
-        case 20:
-        case 21:
-        case 22:
-        case 23:
-            return 'Evening';
-    };
-};
+    const hours = new Date().getHours();
+    if (hours < 6)  return 'Night';
+    if (hours < 12) return 'Morning';
+    if (hours < 18) return 'Afternoon';
+    return 'Evening';
+}
+
 function showGreeting() {
     const timeOfDay = getTimeOfDay();
     const greetingText = `Good ${timeOfDay},`;
